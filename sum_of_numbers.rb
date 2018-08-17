0c5cc3b2bb989fb98f77b5b86d822b59af6bcc3c
JamisonOrdway <jamisonordway@gmail.com>
2018-08-17 10:27:13 -0600

    completes while loop

--- sum_of_numbers.rb
+++ sum_of_numbers.rb
@@ -2,9 +2,21 @@ require 'pry'
 
 sum = 0
 numbers = [1,2,3]
-  
+
+
 for number in numbers
   sum += number
 end
 
 puts sum
+
+@sum_2 = 0
+numbers_2 = [2,2,4]
+i = 0
+
+while (i < numbers.length)
+  @sum_2 += numbers_2[i]
+  i += 1
+end
+
+puts @sum_2
