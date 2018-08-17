a458af40a6fb92a9e821d56c07cfef137c3e3671
JamisonOrdway <jamisonordway@gmail.com>
2018-08-17 10:32:42 -0600

    Completes recursion

--- sum_of_numbers.rb
+++ sum_of_numbers.rb
@@ -1,14 +1,17 @@
 require 'pry'
 
+# USING FOR LOOP 
+
 sum = 0
 numbers = [1,2,3]
-
 
 for number in numbers
   sum += number
 end
 
 puts sum
+
+# USING WHILE LOOP 
 
 @sum_2 = 0
 numbers_2 = [2,2,4]
@@ -20,3 +23,17 @@ while (i < numbers.length)
 end
 
 puts @sum_2
+
+# USING RECURSION
+
+
+def sum(array)
+  if array.empty?
+    0
+  else 
+    number = array.pop
+    return number + sum(array)
+  end
+end
+
+puts sum([5,5,5])
